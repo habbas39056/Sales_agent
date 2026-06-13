7a37c28a54434a0d62f78a63578e8b16efe24b23
HussainAbbas51 <hussainabbas.39056@gmail.com>
2026-06-13 18:36:30 +0500

    Fix Express 5 routing error by replacing wildcard GET with USE middleware

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use('/api/banks', require('./routes/banks'));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Fallback for React Router (Single Page Application)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
